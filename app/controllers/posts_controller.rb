e198e7ea45f406ac518c829ea08ada4481d92262
Matt Gordon <bigmattsize21@gmail.com>
2020-11-05 04:04:52 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,11 +11,7 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to post_path(@post)
-  end
+  
  
 
   def update
