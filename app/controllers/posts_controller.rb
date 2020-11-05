4b69413d99c4bc6ad184f674408b9072154ead8a
Matt Gordon <bigmattsize21@gmail.com>
2020-11-05 04:04:43 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,11 @@ class PostsController < ApplicationController
   end
  
 
-  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params(:title))
+    redirect_to post_path(@post)
+  end
 
 	def edit
 	  @post = Post.find(params[:id])
